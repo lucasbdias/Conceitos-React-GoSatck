c0102a25ed68da00f1b8e52762166b36cf732247
lucasbdias <lucasbrzdias@gmail.com>
2020-08-24 08:39:16 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+            <Header title="Projects" />
+        </>
+    );
+}
+
+export default App;
