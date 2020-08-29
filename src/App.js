e492a6a1b216c390fcf8e3c3eb7f2c3b45bb31e7
lucasbdias <lucasbrzdias@gmail.com>
2020-08-29 13:18:28 -0300

    finalizado a aula sobre conceitos de react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+//import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Lucas Braz"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
     return (
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
             <Header title="Projects" />
+
+            {/*<img src={backgroundImage} alt="background" width={350} />*/}
+
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,21 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
+            },
+            {
+                //condições para arquivos de imagens com estas extensões e Case Insensitive
+                test: /.*\.(gif|png|jpe?g)$/i,
+                use: {
+                    loader: 'file-loader',
+                }
             }
         ]
     },
